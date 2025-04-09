cdfe4030646fd6266918ff13382e4ab7e05b3553
ghazi <ghazi290902@gmail.com>
2025-04-09 19:17:06 +0100

    Implement auth layout and logic also adding sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3b8909e9f4ed9a2ccc16836ea2ac3974@o4509124070277120.ingest.de.sentry.io/4509124072177744",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
